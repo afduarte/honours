9d0eaf6cd187c4a4bdb86c352d4a77b69a375ca3
afduarte <a.fduarte1@gmail.com>
2020-04-04 21:24:11 +0100

    Added instructions to user step in project wizard

--- matac-frontend/src/views/Admin/ProjectWizard/Steps/Three.vue
+++ matac-frontend/src/views/Admin/ProjectWizard/Steps/Three.vue
@@ -1,22 +1,33 @@
 <template lang="pug">
-  .manage-users
-    user-list(:users="userList", :selected="this.activeProject.Users",
-      :clickable="true",
-      @user-click="addUser")
-    div
-      user-form(@submit="genUsers")
-    .nav
-      button(@click="prevStep") Previous
-      button(@click="nextStep") Next
+  .wrapper
+    instructions
+      p To associate users to the project simply click the row in the table that represents the
+        |  user you want to add.
+      p If you need to generate new users, use the panel on the right.
+        |  Type the number of users you want to generate and click the generate button.
+      p Currently M ATAC does not provide you with a way of communicating with the users,
+        |  so if you want to send them their pin, you might want to write down the generated pins.
+        |  The list of users will always be available from the
+        router-link.regular-link(to="/admin/manage-users") manage users panel
+    .manage-users
+      user-list(:users="userList", :selected="this.activeProject.Users",
+        :clickable="true",
+        @user-click="addUser")
+      div
+        user-form(@submit="genUsers")
+      .nav
+        button(@click="prevStep") Previous
+        button(@click="nextStep") Next
 </template>
 
 <script>
 import { mapActions, mapState, mapGetters } from 'vuex';
 import UserList from '@/components/UserList.vue';
 import UserForm from '@/components/UserForm.vue';
+import Instructions from '@/components/Instructions.vue';
 
 export default {
-  components: { UserList, UserForm },
+  components: { UserList, UserForm, Instructions },
   props: ['project'],
   name: 'LinkUsers',
   computed: {
@@ -71,6 +82,7 @@ export default {
   grid-column-end: 3;
 }
 .manage-users {
+  padding-top:10px;
   display: grid;
   gap: 20px;
   grid-template-columns: 8fr 1fr;
@@ -78,4 +90,13 @@ export default {
     margin-top: 50px;
   }
 }
+.regular-link{
+    border:none;
+    padding: 0;
+    padding-left: 2px;
+    background: none;
+    &:hover{
+      background: none;
+    }
+  }
 </style>
